de4efac9a62f67ffb2088d3ab967bda731004d45
John T Day <john.day@razorfish.com>
2015-02-16 21:29:23 -0600

    original

--- client/app/app.entry.js
+++ client/app/app.entry.js
@@ -1,11 +1,11 @@
 Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+  passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
 
 Meteor.startup(function() {
   return AccountsEntry.config({
-    privacyUrl: '/privacy-policy',
-    termsUrl: '/terms-of-use',
+    privacyUrl: '/privacy',
+    termsUrl: '/eula',
     homeRoute: '/',
     dashboardRoute: '/',
     profileRoute: 'profile',
