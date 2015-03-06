7ac099c159f1733556e2f2eeb224b94dcb791247
John T Day <john.day@razorfish.com>
2015-03-06 10:35:43 -0600

    filter

--- client/views/diagram/sequence/segDgmList/list/seqDgmList.js
+++ client/views/diagram/sequence/segDgmList/list/seqDgmList.js
@@ -1,3 +1,29 @@
+var regexQuery = function (searchText) {
+    return {$regex: searchText, $options: 'i'};
+};
+var doFilter = function(){
+    var isStar = $('#starredID').prop( "checked" );
+    var text = $('#searchTextID').val();
+
+    // STORE VALUES
+    AppProperties.diagrams.filters.isStar = isStar;
+    AppProperties.diagrams.filters.text = text;
+
+    // CREATE QUERY
+    var filters = {};
+    if (isStar)
+        filters.starredBy = Meteor.userId();
+    if (text && text.length > 0)
+        filters.title = regexQuery(text);
+
+    console.log('query='+JSON.stringify(filters));
+
+    // RUN
+    DiagramPages.set({
+        filters: filters
+        ,sort: {title: 1}
+    });
+};
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.seqDgmList.helpers({
 });
@@ -11,49 +37,16 @@ Template.seqDgmList.destroyed = function() {
 Template.seqDgmList.events({
     //'click input:radio[name=starredRadioName]': function(e){
     'click #starredID': function(e){
-        var isStar = $(e.target).prop( "checked" );
-        AppProperties.diagrams.filters.isStar = isStar;
-        console.log('isStar=' + isStar);
-
-
-
-        // CREATE QUERY
-        var filters = {};
-        if (isStar)
-            filters.starredBy = Meteor.userId();
-
-        // RUN
-        DiagramPages.set({
-            filters: filters
-            ,sort: {
-                title: 1
-            }
-        });
+        doFilter();
+    },
+    'keyup #searchTextID': function(e) {
+        e.preventDefault();
+        doFilter();
     }
 });
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.seqDgmList.rendered = function() {
-    var isStar = AppProperties.diagrams.filters.isStar;
-    var text = AppProperties.diagrams.filters.text;
-    var f = DiagramPages.filters;
-    //var isStar = _.isMatch( filters, {starredBy: Meteor.userId()} );
-    //var isStar = _.has( filters, 'starredBy');
-
-    // SET INPUT
-    $('#starredID').prop( "checked", isStar );
-
-    // CREATE QUERY
-    var filters = {};
-    if (isStar)
-        filters.starredBy = Meteor.userId();
-
-    // RUN
-    DiagramPages.set({
-        filters: filters
-        ,sort: {
-            title: 1
-        }
-    });
-
+    doFilter();
+    $('#starredID').focus();
 };
 /*------------------------------------------------------------------------------------------------------------------------------*/
