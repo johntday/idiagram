516bee91fed5902254fa44cdc4a64378b7eca9b8
John T Day <john.day@razorfish.com>
2015-03-06 10:05:23 -0600

    filter

--- client/views/diagram/sequence/segDgmList/list/seqDgmList.js
+++ client/views/diagram/sequence/segDgmList/list/seqDgmList.js
@@ -9,20 +9,51 @@ Template.seqDgmList.destroyed = function() {
 };
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.seqDgmList.events({
-    'click input:radio[name=starredRadioName]': function(e){
-        console.log('clicked=' + $(e.target).val());
+    //'click input:radio[name=starredRadioName]': function(e){
+    'click #starredID': function(e){
+        var isStar = $(e.target).prop( "checked" );
+        AppProperties.diagrams.filters.isStar = isStar;
+        console.log('isStar=' + isStar);
 
-        //DiagramPages.filters = {starredBy: Meteor.userId()};
 
+
+        // CREATE QUERY
+        var filters = {};
+        if (isStar)
+            filters.starredBy = Meteor.userId();
+
+        // RUN
         DiagramPages.set({
-            perPage: 10,
-            sort: {
-                title: -1
+            filters: filters
+            ,sort: {
+                title: 1
             }
         });
     }
 });
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.seqDgmList.rendered = function() {
+    var isStar = AppProperties.diagrams.filters.isStar;
+    var text = AppProperties.diagrams.filters.text;
+    var f = DiagramPages.filters;
+    //var isStar = _.isMatch( filters, {starredBy: Meteor.userId()} );
+    //var isStar = _.has( filters, 'starredBy');
+
+    // SET INPUT
+    $('#starredID').prop( "checked", isStar );
+
+    // CREATE QUERY
+    var filters = {};
+    if (isStar)
+        filters.starredBy = Meteor.userId();
+
+    // RUN
+    DiagramPages.set({
+        filters: filters
+        ,sort: {
+            title: 1
+        }
+    });
+
 };
 /*------------------------------------------------------------------------------------------------------------------------------*/
