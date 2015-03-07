322951b99b79920a37360f275e3741dd50de50e4
John T Day <john.day@razorfish.com>
2015-03-06 20:15:19 -0600

    search

--- client/views/diagram/sequence/segDgmList/list/seqDgmList.js
+++ client/views/diagram/sequence/segDgmList/list/seqDgmList.js
@@ -6,16 +6,19 @@ var doFilter = function(){
     var isStar = $('#starredID').prop( "checked" );
     var text = $('#searchTextID').val();
     var private = $('input:radio[name=private]:checked').val();
+    var hideOtherPublic = $('#hideOtherPublicID').prop( "checked" );
 
-    console.log('INPUT: ', isStar, text, private);
+    console.log('INPUT: ', isStar, text, private, hideOtherPublic);
 
     // STORE VALUES
     AppProperties.diagrams.filters.isStar = isStar;
     AppProperties.diagrams.filters.text = text;
     AppProperties.diagrams.filters.private = private;
+    AppProperties.diagrams.filters.hideOtherPublic = hideOtherPublic;
 
     // CREATE QUERY
     var filters = {};
+
     if (isStar)
         filters.starredBy = Meteor.userId();
     if (text && text.length > 0)
@@ -24,6 +27,10 @@ var doFilter = function(){
         filters.private = true;
     else if (private=='public')
         filters.private = false;
+    if (hideOtherPublic)
+        filters.userId = Meteor.userId();
+    else
+        filters.$or = [{userId: Meteor.userId()}, {private: false}];
 
     console.log('query='+JSON.stringify(filters));
 
@@ -45,7 +52,7 @@ Template.seqDgmList.destroyed = function() {
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.seqDgmList.events({
     //'click input:radio[name=starredRadioName]': function(e){
-    'click #starredID, keyup #searchTextID, click input:radio[name=private]': function(e){
+    'click #starredID, keyup #searchTextID, click input:radio[name=private], click #hideOtherPublicID': function(e){
         doFilter();
     //},
     //'keyup #searchTextID': function(e) {
@@ -63,6 +70,7 @@ Template.seqDgmList.rendered = function() {
     $('#starredID').prop( "checked", AppProperties.diagrams.filters.isStar );
     $('#searchTextID').val(AppProperties.diagrams.filters.text);
     $('#'+AppProperties.diagrams.filters.private+'ID').prop('checked', true);
+    $('#hideOtherPublicID').prop( "checked", AppProperties.diagrams.filters.hideOtherPublic );
 
     doFilter();
 
