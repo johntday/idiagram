11e189e55b3b87670578a76ac49a9995d3bc5bb7
John T Day <john.day@razorfish.com>
2015-03-09 10:20:07 -0500

    sort

--- client/views/diagram/sequence/segDgmList/list/seqDgmList.js
+++ client/views/diagram/sequence/segDgmList/list/seqDgmList.js
@@ -7,14 +7,16 @@ var doFilter = function(){
     var text = $('#searchTextID').val();
     var private = $('input:radio[name=private]:checked').val();
     var hideOtherPublic = $('#hideOtherPublicID').prop( "checked" );
-
-    //console.log('INPUT: ', isStar, text, private, hideOtherPublic);
+    var sortBy = $('#sortBtnID').val();
+    var sortDir = parseInt( $('input:radio[name=sortDir]:checked').val() );
 
     // STORE VALUES
     AppProperties.diagrams.filters.isStar = isStar;
     AppProperties.diagrams.filters.text = text;
     AppProperties.diagrams.filters.private = private;
     AppProperties.diagrams.filters.hideOtherPublic = hideOtherPublic;
+    AppProperties.diagrams.sort.sortBy = sortBy;
+    AppProperties.diagrams.sort.sortDir = sortDir;
 
     // CREATE QUERY
     var filters = {};
@@ -32,12 +34,16 @@ var doFilter = function(){
     else if (isAdmin(Meteor.user()) == false)
         filters.$or = [{userId: Meteor.userId()}, {private: false}];
 
+    var sort = {};
+    sort[sortBy] = sortDir;
+
     //console.log('query='+JSON.stringify(filters));
+    //console.log('sort='+JSON.stringify(sort));
 
     // RUN
     DiagramPages.set({
         filters: filters
-        ,sort: {title: 1}
+        ,sort: sort
     });
 };
 /*------------------------------------------------------------------------------------------------------------------------------*/
@@ -51,18 +57,20 @@ Template.seqDgmList.destroyed = function() {
 };
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.seqDgmList.events({
-    'click #starredID, keyup #searchTextID, click input:radio[name=private], click #hideOtherPublicID': function(e){
+    'click #starredID, keyup #searchTextID, click input:radio[name=private], click #hideOtherPublicID, click input:radio[name=sortDir], change #sortBtnID': function(e){
         doFilter();
     }
-
 });
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.seqDgmList.rendered = function() {
+    $("form").submit(function() { return false; });
     //INIT
     $('#starredID').prop( "checked", AppProperties.diagrams.filters.isStar );
     $('#searchTextID').val(AppProperties.diagrams.filters.text);
     $('#'+AppProperties.diagrams.filters.private+'ID').prop('checked', true);
     $('#hideOtherPublicID').prop( "checked", AppProperties.diagrams.filters.hideOtherPublic );
+    $('#sortBtnID').val(AppProperties.diagrams.sort.sortBy);
+    $('#sort'+AppProperties.diagrams.sort.sortDir+'ID').prop('checked', true);
 
     doFilter();
 
