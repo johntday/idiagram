a412ad0101f7c085107dd4736f70686d184d44ab
John T Day <john.day@razorfish.com>
2015-04-18 18:36:00 -0500

    view

--- client/views/context/layout/_ctxDgm.js
+++ client/views/context/layout/_ctxDgm.js
@@ -30,7 +30,8 @@ Template._ctxDgm.rendered = function() {
         title: 'Context Diagram Help'
         ,content: '<table class="table table-striped table-condensed table-bordered"><tbody>'+
         '<tr><td><u>Drag nodes</u> to re-jigger the diagram.</td></tr>'+
-        '<tr><td><u>Double-click</u> to re-center diagram</td></tr>' +
+        '<tr><td><u>Drag background</u> to pan the diagram.</td></tr>'+
+        '<tr><td><u>Double-click</u> to re-center the diagram</td></tr>' +
         '<tr><td>Use <u>mouse-wheel</u> to zoom-in and out</td></tr>'+
         '</tbody></table>'
 
@@ -41,7 +42,8 @@ Template._ctxDgm.rendered = function() {
         title: 'Summary Diagram Help'
         ,content: '<table class="table table-striped table-condensed table-bordered"><tbody>'+
         '<tr><td><u>Drag nodes</u> to re-jigger the diagram.  Sometimes you need to jerk them to a good spot to uncross lines.</td></tr>'+
-        '<tr><td><u>Double-click</u> to re-center diagram</td></tr>' +
+        '<tr><td><u>Drag background</u> to pan the diagram.</td></tr>'+
+        '<tr><td><u>Double-click</u> to re-center the diagram</td></tr>' +
         '<tr><td>Use <u>mouse-wheel</u> to zoom-in and out</td></tr>'+
         '</tbody></table>'
         ,placement: 'right'
@@ -51,6 +53,7 @@ Template._ctxDgm.rendered = function() {
         title: 'Grid Diagram Help'
         ,content: '<table class="table table-striped table-condensed table-bordered"><tbody>'+
         '<tr><td><u>Drag nodes</u> is broken</td></tr>'+
+        '<tr><td><u>Drag background</u> is broken</td></tr>'+
         '<tr><td><u>Double-click</u> is broken</td></tr>'+
         '<tr><td>Use <u>mouse-wheel</u> is broken</td></tr>'+
         '</tbody></table>'
